176d8f98d9b22399cbbcc2c92e5cc1291854d975
johny <johnyjames9496@gmail.com>
2026-04-18 12:23:20 +0530

    Day 6: Protected routes, JWT middleware, get and update profile endpoints

--- app/utils/jwt_handler.py
+++ app/utils/jwt_handler.py
@@ -1,21 +1,19 @@
-from jose import JWTError,jwt
-from datetime import datetime,timezone,timedelta
-from dotenv import load_dotenv
-import os
-load_dotenv
-SECRET_KEY=os.getenv("SECRET_KEY")
+from jose import JWTError, jwt
+from datetime import datetime, timezone, timedelta
+from app.config import SECRET_KEY
+
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 
-def create_access_token(data:dict)->str:
-  to_encode = data.copy()
-  expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-  to_encode.update({"exp":expire})
-  return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
-def decode_access_token(token:str)->dict:
-  try:
-    payload = jwt.decode(token,SECRET_KEY,algorithm=[ALGORITHM])
-    return payload
-  except JWTError:
-    return None
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
